aa414418d9a4c670f812e827ece54ce371a41a9e
Suyashendra <suyashendrarathore9039@gmail.com>
2024-02-26 19:47:51 +0530

    Day2

---
+++ Day2_Pattern/P2InvertedStarPattern.java
@@ -0,0 +1,22 @@
+/*
+Inverted Star Pattern
+****
+***
+**
+*
+
+ */
+
+import java.util.*;
+public class P2InvertedStarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=n;i>0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
